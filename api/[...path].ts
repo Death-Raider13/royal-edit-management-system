aa5889000f630be2f5350025f03e78b17395fcfa
Death-Raider13 <lateefedidi4@gmail.com>
2026-08-20 00:00:47 +0100

    n

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,6 +1,6 @@
 import app from "../server";
 
 export default function handler(req: any, res: any) {
-  // Express apps are compatible as request handlers — forward the Vercel request to the Express app.
-  return app(req, res);
+    // Express apps are compatible as request handlers — forward the Vercel request to the Express app.
+    return app(req, res);
 }
